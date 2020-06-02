25998d2b226db1778eb57fdd807059e2daecc19a
Edgars Hartmanis <edgarhart@inbox.lv>
2020-06-02 21:55:58 +0300

    Minor Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div className="ui raised segment">
-      <p>
-        <h3>This is Portfolio Gallery App based on React library for bachelor's thesis.</h3>
-        <h4>Made by Edgars Hartmanis</h4>
-      </p>
+      <h3>This is Portfolio Gallery App based on React library for bachelor's thesis.</h3>
+      <h4>Made by Edgars Hartmanis</h4>
     </div>
   )
 };
